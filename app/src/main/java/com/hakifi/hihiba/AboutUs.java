36752e9cc0c48cfb1ad1ee2a67e84b064eb33c96
kimiginting <kimiginting@hotmail.com>
2023-11-02 12:47:55 +0700

    full design of HiHiBa *fix

--- app/src/main/java/com/hakifi/hihiba/AboutUs.java
+++ app/src/main/java/com/hakifi/hihiba/AboutUs.java
@@ -3,12 +3,18 @@ package com.hakifi.hihiba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class AboutUs extends AppCompatActivity {
 
+    ImageView imgKipli;
+    ImageView imgHaff;
+    ImageView imgKimi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +22,9 @@ public class AboutUs extends AppCompatActivity {
         getSupportActionBar().hide();
 
         Button btnBack = findViewById(R.id.back);
+        imgKipli = (ImageView) findViewById(R.id.kipliIG);
+        imgHaff = (ImageView) findViewById(R.id.haffIG);
+        imgKimi = (ImageView) findViewById(R.id.kimiIG);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +33,30 @@ public class AboutUs extends AppCompatActivity {
                 startActivity(explicit);
             }
         });
+
+        imgKipli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/erxyon/"));
+                startActivity(implicit);
+            }
+        });
+
+        imgHaff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/hafidzmrizky/"));
+                startActivity(implicit);
+            }
+        });
+
+        imgKimi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/kimiginting/"));
+                startActivity(implicit);
+            }
+        });
+
     }
 }
